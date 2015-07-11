2005150c290d72a2e3220c82569fd423b604b912
ashwathiyer <ashwathdr@gmail.com>
2015-07-12 00:18:48 +0530

    Commited

--- src/main/java/com/praxify/hack/HomeController.java
+++ src/main/java/com/praxify/hack/HomeController.java
@@ -5,11 +5,23 @@ import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.praxify.hack.DAO.SpringMongoConfig;
+import com.praxify.hack.template.Patient;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +47,20 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	
+	@RequestMapping(value="/registerPatient",method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+    public void addPatient(){
+        logger.info("Rest call to test Crud operations");
+        Patient patient=new Patient("Ashwath", "Iyer","01-05-1989" ,"ashwathdr@gmail.com", new Date().toString(),"9738482973",  "123123");
+        logger.info("Trying to save patient details: "+patient);
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig.class);
+        MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+        mongoOperation.save(patient);
+        System.out.println("Patient Details :"+mongoOperation.findAll(Patient.class));
+        logger.info("saved Patient is: "+mongoOperation.findAll(Patient.class));
+    }
+    
+	
 	
 }
---
+++ src/main/java/com/praxify/hack/template/Patient.java
@@ -0,0 +1,30 @@
+package com.praxify.hack.template;
+
+public class Patient {
+
+	private String firstName;
+	private String lastName;
+	private String dob;
+	private String email;
+	private String date;
+	private String mobile;
+	private String id;
+	
+	
+	public Patient() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Patient(String firstName, String lastName, String dob, String email, String date, String mobile, String id){
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.email = email;
+		this.mobile = mobile;
+		this.id = id;
+		
+	}
+	
+	
+}
